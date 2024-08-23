a131f7190e22e987cb1870ec13e108c04e44d0d5
Bakijazzman <obafemiolabayo@gmail.com>
2024-08-23 14:29:49 +0100

    shipping address and page updated

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,8 @@
-from .views import payment_success
+from .views import payment_success, checkout
 from django.urls import path
 
 
 urlpatterns = [
     path('payment_success', payment_success, name="payment_success"),
+    path('checkout', checkout, name="checkout"),
 ]
